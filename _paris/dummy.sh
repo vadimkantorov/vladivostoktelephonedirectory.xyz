5f3ce1c6a654955abdcb29f4c0f2a3afed1dc6b2
Vadim Kantorov <vadimkantorov@gmail.com>
2014-10-29 01:49:42 +0100

    ...

--- _paris/dummy.sh
+++ _paris/dummy.sh
@@ -2,6 +2,19 @@ for i in {1..9}
 do
 	for j in {0..9}
 	do
-		cp 2012-01-01-resto_00.md 2012-01-01-resto_$i$j.md
+		cp 2012-01-01-resto_00.md 2012-01-02-resto_$i$j.md
 	done
 done
+
+sed -i 's/name/tags: drink\nname/g' 2012-01-02*
+
+for i in {1..9}
+do
+	for j in {0..9}
+	do
+		cp 2012-01-01-resto_00.md 2012-01-03-resto_$i$j.md
+	done
+done
+
+
+sed -i 's/name/tags: eat\nname/g' 2012-01-03*
